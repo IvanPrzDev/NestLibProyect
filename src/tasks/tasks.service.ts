3eca6a9d0d87aa32f5fdb19f4e63ea454e664324
IvanDev <ivanperezrebollo@hotmail.com>
2025-10-03 13:57:04 +0200

    Initial commit: NestJS project with Tasks module

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.entity';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [
+    {
+      id: '1',
+      title: 'First Task',
+      description: 'This is the first task',
+      status: TaskStatus.PENDING,
+    },
+  ];
+
+  getAllTasks() {
+    return this.tasks;
+  }
+  createTask(title: string, description: string) {
+    const task = {
+      id: uuidv4(),
+      title,
+      description,
+      status: TaskStatus.PENDING,
+    };
+
+    this.tasks.push(task);
+
+    return task;
+  }
+  updateTask() {}
+  deleteTask() {}
+}
